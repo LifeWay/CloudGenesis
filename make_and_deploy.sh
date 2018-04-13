19281d96476585dfff2b6e57898729e9ef1cbe92
aherrin <adam.herrington@lifeway.com>
2018-04-13 15:54:02 -0500

    removed account numbers/etc from test files, added zips to ignore, removed deploy helper script

--- make_and_deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sh make.sh && \
-aws cloudformation package --template template.yaml --s3-prefix cloudops/s3-event --s3-bucket test-sam-codestore --output-template-file packaged.yaml && \
-aws cloudformation deploy --template-file packaged.yaml --stack-name cloudops-cf-automation-poc-with-iam --capabilities CAPABILITY_IAM --parameter-overrides AllowIAMCapabilities=true AccountIDArns="arn:aws:iam::567419588983:root" ExternalNotificationSNSArn="arn:aws:sns:us-east-1:463625765753:rm-cross-acct-test" --region us-east-1
